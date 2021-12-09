9046e8dd2cf76a7c5e1d352a444ba8e0df428458
Anthony Immenschuh <anthonyimmenschuh@Anthonys-MacBook-Pro-2.local>
2021-12-09 05:36:51 -0600

    added nexr fit yessirrrrrrrrrrrrrrrr bing bong

--- project2.c
+++ project2.c
@@ -126,7 +126,7 @@ void nextFIT(struct memory *allMemory, struct memory process, struct memoryInfo 
   while (i < length && process.size <= allMemoryInfo->fullLength - allMemoryInfo->totalSize && length > 0) { // loop to find best slot
 
 
-    if (i == allMemoryInfo->listLength-1  && process.size <= allMemoryInfo->fullLength-allMemory[i].tail-1 && flag == 0) { //if empty space is last
+    if (i == allMemoryInfo->listLength  && process.size <= allMemoryInfo->fullLength-allMemory[i].tail-1 && flag == 0) { //if empty space is last
       temp.head = allMemory[i].tail+1;
       temp.tail = temp.head+process.size-1;
       flag=1;
